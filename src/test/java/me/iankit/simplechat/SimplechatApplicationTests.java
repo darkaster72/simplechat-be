ddd33014b211bb450cca615f3c26dc5d8f5e9584
Ankit Sharma <sankit72@gmail.com>
2022-09-03 09:33:02 +0530

    initial commit

---
+++ src/test/java/me/iankit/simplechat/SimplechatApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iankit.simplechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
