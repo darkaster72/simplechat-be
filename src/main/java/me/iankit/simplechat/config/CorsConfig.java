59c7b1e82d0c4d6e22397340c9d561d642ed10be
Ankit Sharma <sankit72@gmail.com>
2022-09-04 18:04:08 +0530

    updated the cors config for reactive

--- src/main/java/me/iankit/simplechat/config/CorsConfig.java
+++ src/main/java/me/iankit/simplechat/config/CorsConfig.java
@@ -1,13 +1,11 @@
 package me.iankit.simplechat.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebFluxConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
